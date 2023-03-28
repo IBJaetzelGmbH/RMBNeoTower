5894628b636c79db9957d3751a4d8b2f4264a77e
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-03-28 12:28:05 +0200

    Quantity fehlte

--- Read and Write/module.php
+++ Read and Write/module.php
@@ -9,10 +9,11 @@ require_once __DIR__ . '/../libs/ModBusBaseModule.php';
         const PREFIX = 'RNTRW';
 
         public static $Variables = [
-            ['Elektrische Soll Leistung', VARIABLETYPE_FLOAT, 'TRMBNeoTower.kW', 0.1,  0x6000, 6, 6,  true],
-			['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x400, 5, 5,  true],
-			['Start Stopp Extern', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4001, 5, 5,  true],
-			['Sperre', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4002, 5, 5,  true],
+
+            ['Elektrische Soll Leistung', VARIABLETYPE_FLOAT, 'TRMBNeoTower.kW', 0.1,  0x6000, 1, 6, 6,  true],
+            ['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x400, 5, 1, 5,  true],
+            ['Start Stopp Extern', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4001, 5, 1, 5,  true],
+            ['Sperre', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4002, 5, 5,  true],
         ];
 
         public function Destroy()
