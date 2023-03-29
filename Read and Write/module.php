ab6270a6b96eb47cf4ffeb145c775baed40e8b5d
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-03-29 08:37:30 +0200

    Fix Adress

--- Read and Write/module.php
+++ Read and Write/module.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../libs/ModBusBaseModule.php';
         public static $Variables = [
             //Name/Ident,  VarType, Profil, Factor, Address, Function, Quantity, FunctionW
             ['Elektrische Soll Leistung', VARIABLETYPE_FLOAT, 'RMBNeoTower.kW', 0.1,  0x6000,  3, 1, 6,  true],
-            ['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x400, 1, 1, 5,  true],
+            ['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4000, 1, 1, 5,  true],
             ['Start Stopp Extern', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4001, 1, 1, 5,  true],
             ['Sperre', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4002, 1, 1, 5,  true],
         ];
