7ac37d146c0081b9e6529d51b32fa9f0dc7ff938
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-03-29 08:34:10 +0200

    RMBneoTowerReadandWrite

--- Read and Write/module.php
+++ Read and Write/module.php
@@ -9,11 +9,11 @@ require_once __DIR__ . '/../libs/ModBusBaseModule.php';
         const PREFIX = 'RNTRW';
 
         public static $Variables = [
-
-            ['Elektrische Soll Leistung', VARIABLETYPE_FLOAT, 'RMBNeoTower.kW', 0.1,  0x6000,  6, 1, 6,  true],
-            ['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x400, 5, 1, 5,  true],
-            ['Start Stopp Extern', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4001, 5, 1, 5,  true],
-            ['Sperre', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4002, 5, 1, 5,  true],
+            //Name/Ident,  VarType, Profil, Factor, Address, Function, Quantity, FunctionW
+            ['Elektrische Soll Leistung', VARIABLETYPE_FLOAT, 'RMBNeoTower.kW', 0.1,  0x6000,  3, 1, 6,  true],
+            ['Anforderung Entladepumpe', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x400, 1, 1, 5,  true],
+            ['Start Stopp Extern', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4001, 1, 1, 5,  true],
+            ['Sperre', VARIABLETYPE_BOOLEAN, '~Switch', null,  0x4002, 1, 1, 5,  true],
         ];
 
         public function Destroy()
